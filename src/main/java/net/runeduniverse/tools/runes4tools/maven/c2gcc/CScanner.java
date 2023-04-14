5ac9b1c243cf637788d8ef1f5d4b7372e29eae81
VenaNocta <venanocta@gmail.com>
2023-04-14 14:22:35 +0200

    implemented ICompilerRuntime plexus-requirement

--- src/main/java/net/runeduniverse/tools/runes4tools/maven/c2gcc/CScanner.java
+++ src/main/java/net/runeduniverse/tools/runes4tools/maven/c2gcc/CScanner.java
@@ -2,15 +2,21 @@ package net.runeduniverse.tools.runes4tools.maven.c2gcc;
 
 import org.apache.maven.plugin.logging.Log;
 import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
 
+import net.runeduniverse.tools.maven.compiler.api.ICompilerRuntime;
 import net.runeduniverse.tools.maven.compiler.api.IReferenceScanner;
 
 @Component(role = IReferenceScanner.class, hint = "c2gcc")
 public class CScanner implements IReferenceScanner{
 
+	@Requirement
+	protected ICompilerRuntime runtime;
+
 	@Override
 	public boolean logInfo(Log log) {
-		log.info("C Sacnner  - Hello World!");
+		log.info("C Scanner  - Hello World!");
+		log.info("scanning src for c files: " + this.runtime.getSourceDirectory());
 		// TODO Auto-generated method stub
 		return false;
 	}
