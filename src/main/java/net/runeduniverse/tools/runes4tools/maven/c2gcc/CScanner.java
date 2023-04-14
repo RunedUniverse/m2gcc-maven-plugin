21dbfb8d66c7d9fc71c6e4a9195011158a07871d
VenaNocta <venanocta@gmail.com>
2023-04-14 22:12:29 +0200

    updated CScanner to use CurrentContextUtils

--- src/main/java/net/runeduniverse/tools/runes4tools/maven/c2gcc/CScanner.java
+++ src/main/java/net/runeduniverse/tools/runes4tools/maven/c2gcc/CScanner.java
@@ -1,20 +1,26 @@
 package net.runeduniverse.tools.runes4tools.maven.c2gcc;
 
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.logging.Log;
 import org.codehaus.plexus.component.annotations.Component;
 import org.codehaus.plexus.component.annotations.Requirement;
 
 import net.runeduniverse.tools.maven.compiler.api.ICompilerRuntime;
 import net.runeduniverse.tools.maven.compiler.api.IReferenceScanner;
+import net.runeduniverse.tools.maven.compiler.api.mojos.CurrentContextUtils;
 
 @Component(role = IReferenceScanner.class, hint = "c2gcc")
-public class CScanner implements IReferenceScanner{
+public class CScanner implements IReferenceScanner {
 
 	@Requirement
+	private MavenSession mvnSession;
+
 	protected ICompilerRuntime runtime;
 
 	@Override
 	public boolean logInfo(Log log) {
+		this.runtime = CurrentContextUtils.lookupComponent(this.mvnSession, ICompilerRuntime.class);
+
 		log.info("C Scanner  - Hello World!");
 		log.info("scanning src for c files: " + this.runtime.getSourceDirectory());
 		// TODO Auto-generated method stub
