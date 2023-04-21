4cc26492cfeca5f98a65e71c28ce7a2067243a88
VenaNocta <venanocta@gmail.com>
2023-04-21 16:38:19 +0200

    migrated cpp2gcc to m2gcc

---
+++ module-cpp/src/main/java/net/runeduniverse/tools/maven/m2gcc/modules/cpp/CompilerMojo.java
@@ -0,0 +1,22 @@
+package net.runeduniverse.tools.maven.m2gcc.modules.cpp;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+import net.runeduniverse.tools.maven.compiler.api.mojos.ACompilerMojo;
+
+/**
+ * Assembler for GCC
+ * @author VenaNocta
+ *
+ * @phase compiler
+ * @goal compile
+ */
+public class CompilerMojo extends ACompilerMojo {
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		getLog().info("cpp2gcc:compile");
+	}
+
+}
---
+++ module-cpp/src/main/java/net/runeduniverse/tools/maven/m2gcc/modules/cpp/CppScanner.java
@@ -0,0 +1,24 @@
+package net.runeduniverse.tools.maven.m2gcc.modules.cpp;
+
+import org.apache.maven.plugin.logging.Log;
+import org.codehaus.plexus.component.annotations.Component;
+
+import net.runeduniverse.tools.maven.compiler.api.IReferenceScanner;
+
+@Component(role = IReferenceScanner.class, hint = "cpp2gcc")
+public class CppScanner implements IReferenceScanner{
+
+	@Override
+	public boolean logInfo(Log log) {
+		log.info("Cpp Sacnner  - Hello World!");
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean logAnalisis(Log log) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
